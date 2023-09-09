aff4c383d36242b5ee61110994bb83c73818f359
sr08678 <sebastian.rauch@stud.th-deg.de>
2023-09-09 14:16:51 +0200

    yoolink stand funktioniert

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,7 +146,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
     "uhrmannreisen.middleware.WwwRedirectMiddleware",
 ]
 
